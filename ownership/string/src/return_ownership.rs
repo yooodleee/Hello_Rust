e781af190f66e8d8c98a2617874632c060d03dd6
yooodleee <accia25@naver.com>
2025-11-08 18:03:44 +0900

    return value and scope

---
+++ ownership/string/src/return_ownership.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let s1 = String::from("hello");
+    let (s2, len) = calculate_length(s1);
+
+    println!("The length of '{}' is {}.", s2, len);
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    len length = s.len();  // len(): String
+    (s, length)
+}
